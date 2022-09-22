2c8b9aab4a942dd058cb9a5de10ac5c7ded9c5e6
Boyan Todorov <r0b0engi2T@gmail.com>
2022-09-22 07:50:58 +0200

    updates

--- charts.py
+++ charts.py
@@ -1,6 +1,8 @@
 import sqlite3
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from datetime import datetime
+
 
 path = r"S:\Warehouse dept\Inventory control\Reporting and dashboards\Bin's Counting\Cycle Counting\DATABASE\cycle_couting.db"
 
@@ -46,10 +48,14 @@ def get_data_from_database(warehouse):
 for wh in all_warehouses.keys():
         all_warehouses[wh].append(get_data_from_database(wh))
 
-pp = PdfPages('Countings.pdf')
+pp = PdfPages('Report_' + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.pdf')
+
 for wh in all_warehouses.items():
         warehouse = wh[0]
         total = wh[1][0]
         counted =wh[1][1]
+        # print('Counted ' + str(counted[0]))
+        # print('Not Counted ' + str(total - counted[0]))
+        # print('Total ' + total)
         pp.savefig(plotGraph('Counted\n' + str(counted[0]), 'Not Counted\n' + str(total - counted[0]), total, counted[0], warehouse))
 pp.close()
